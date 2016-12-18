2b822865d079c3e1a022c51ac13b3b1bd1a369da
Cédric Belin <cedric@belin.io>
2016-12-18 21:18:06 +0100

    Added the `Author` class

---
+++ lib/src/author.dart
@@ -0,0 +1,54 @@
+part of akismet;
+
+/// Represents the author of a comment.
+class Author {
+
+  /// Creates a new author.
+  Author([this.name, this.email]);
+
+  /// Creates a new author from the specified [map] in JSON format.
+  Author.fromJson(Map<String, String> map) {
+    assert(map != null);
+
+    if (map['comment_author'] != null) name = map['comment_author'];
+    if (map['comment_author_email'] != null) email = map['comment_author_email'];
+    if (map['comment_author_url'] != null) url= Uri.parse(map['comment_author_url']);
+    if (map['user_agent'] != null) userAgent = map['user_agent'];
+    if (map['user_ip'] != null) ipAddress = map['user_ip'];
+    if (map['user_role'] != null) role = map['user_role'];
+  }
+
+  /// The author's mail address.
+  String email;
+
+  /// The author's IP address.
+  String ipAddress;
+
+  /// The author's name.
+  String name;
+
+  /// The author's role.
+  /// If you set it to `"administrator"`, Akismet will always return `false`.
+  String role;
+
+  /// The URL of the author's website.
+  Uri url;
+
+  /// The author's user agent, that is the string identifying the Web browser used to submit comments.
+  String userAgent;
+
+  /// Converts this object to a map in JSON format.
+  Map<String, String> toJson() {
+    var map = {};
+    if (name != null) map['comment_author'] = name;
+    if (email != null) map['comment_author_email'] = email;
+    if (url != null) map['comment_author_url'] = url.toString();
+    if (userAgent != null) map['user_agent'] = userAgent;
+    if (ipAddress != null) map['user_ip'] = ipAddress;
+    if (role != null) map['user_role'] = role;
+    return map;
+  }
+
+  /// Returns a string representation of this object.
+  @override String toString() => 'Author ${JSON.encode(this)}';
+}
