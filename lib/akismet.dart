b254610960b480d28e6960e692a78a01ca9a678f
Cédric Belin <cedric@belin.io>
2016-12-19 22:45:22 +0100

    Added the Web assets

---
+++ lib/akismet.dart
@@ -0,0 +1,15 @@
+/// Prevent comment spam using the Akismet service.
+library akismet;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+part 'src/author.dart';
+part 'src/blog.dart';
+part 'src/client.dart';
+part 'src/comment.dart';
+part 'src/comment_type.dart';
+
+/// The version number of the package.
+const String VERSION = '1.0.0';
