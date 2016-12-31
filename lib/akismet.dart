216e3ab17b5951031e8da37bdb0b6a6be6df07f3
Cédric Belin <cedric@belin.io>
2016-12-31 01:20:24 +0100

    Added the `_dartVersion` and `_packageVersion` constants

--- lib/akismet.dart
+++ lib/akismet.dart
@@ -5,11 +5,16 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:http/http.dart' as http;
+
 part 'src/author.dart';
 part 'src/blog.dart';
 part 'src/client.dart';
 part 'src/comment.dart';
 part 'src/comment_type.dart';
 
+/// The version number of the Dart runtime.
+final String _dartVersion = Platform.version.replaceFirstMapped(new RegExp(r'^(\d+(\.\d+){2}).*'), (match) => '${match[1]}');
+
 /// The version number of the package.
-const String VERSION = '1.0.0';
+const String _packageVersion = '1.0.0';
