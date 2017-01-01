983da10be09333e5c03f7d0b14902d7fe8a76d09
Cédric Belin <cedric@belin.io>
2017-01-01 02:05:20 +0100

    Fixed a typo

--- lib/src/blog.dart
+++ lib/src/blog.dart
@@ -20,7 +20,7 @@ class Blog {
   /// The character encoding for the values included in comments.
   String charset;
 
-  /// The language(s) in use on the blog or site, in ISO 639-1 format, comma-separated.
+  /// The languages in use on the blog or site, in ISO 639-1 format, comma-separated.
   List<String> languages = [];
 
   /// The blog or site URL.
