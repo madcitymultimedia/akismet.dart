ac116c8178a5fee8dffed8cff40654b6f6d93a90
Cédric Belin <cedric@belin.io>
2018-10-14 12:30:20 +0200

    Fixed the unit tests

--- lib/akismet.dart
+++ lib/akismet.dart
@@ -1,20 +1,5 @@
 /// Prevent comment spam using the [Akismet](https://akismet.com) service.
 library akismet;
 
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io' show HttpHeaders;
-import 'package:http/http.dart' as http;
-import 'package:json_annotation/json_annotation.dart';
-import 'src/version.dart';
-
-export 'package:http/http.dart' show ClientException;
-
-part 'akismet.g.dart';
-part 'src/author.dart';
-part 'src/blog.dart';
-part 'src/comment.dart';
-part 'src/client.dart';
-
-/// The version of the current platform.
-String get _platformVersion => '2.0.0';
+export 'src/core.dart';
+export 'src/http.dart';
