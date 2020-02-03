109d802a493579583d9f526d3d878726583815d4
Cédric Belin <cedric@belin.io>
2020-02-03 19:39:37 +0100

    Added the `CheckResult` enumeration

--- lib/src/http/client.dart
+++ lib/src/http/client.dart
@@ -79,3 +79,16 @@ class Client {
     return response.body;
   }
 }
+
+/// Specifies the result of a comment check.
+enum CheckResult {
+
+  /// The comment is not a spam (i.e. a ham).
+  isHam,
+
+  /// The comment is a spam.
+  isSpam,
+
+  /// The comment is a pervasive spam (i.e. it can be safely discarded).
+  isPervasiveSpam
+}
