366e9e78eb16e1854fac45d80509ed0cfde824c9
Cédric Belin <cedric@belin.io>
2020-04-22 02:17:51 +0200

    Updated the tests

--- test/blog_test.dart
+++ test/blog_test.dart
@@ -13,28 +13,28 @@ void main() => group('Blog', () {
 
     test('should return an initialized instance with a non-empty map', () {
       final blog = Blog.fromJson({
-        'blog': 'https://dev.belin.io/akismet.dart',
+        'blog': 'https://github.com/akismet.dart',
         'blog_charset': 'UTF-8',
         'blog_lang': 'en, fr'
       });
 
       expect(blog.charset, equals('UTF-8'));
       expect(blog.languages, orderedEquals(<String>['en', 'fr']));
-      expect(blog.url, equals(Uri.https('dev.belin.io', '/akismet.dart')));
+      expect(blog.url, equals(Uri.https('github.com', '/akismet.dart')));
     });
   });
 
   group('.toJson()', () {
     test('should return only the blog URL with a newly created instance', () {
-      final data = Blog(Uri.https('dev.belin.io', '/akismet.dart')).toJson();
+      final data = Blog(Uri.https('github.com', '/akismet.dart')).toJson();
       expect(data, hasLength(1));
-      expect(data['blog'], equals('https://dev.belin.io/akismet.dart'));
+      expect(data['blog'], equals('https://github.com/akismet.dart'));
     });
 
     test('should return a non-empty map with an initialized instance', () {
-      final data = Blog(Uri.https('dev.belin.io', '/akismet.dart'), charset: 'UTF-8', languages: ['en', 'fr']).toJson();
+      final data = Blog(Uri.https('github.com', '/akismet.dart'), charset: 'UTF-8', languages: ['en', 'fr']).toJson();
       expect(data, hasLength(3));
-      expect(data['blog'], equals('https://dev.belin.io/akismet.dart'));
+      expect(data['blog'], equals('https://github.com/akismet.dart'));
       expect(data['blog_charset'], equals('UTF-8'));
       expect(data['blog_lang'], equals('en,fr'));
     });
