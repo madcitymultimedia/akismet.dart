ab6bb6dd79e3c89244121ecb3c15eb38a9c9c1e4
Cédric Belin <cedric@belin.io>
2020-12-04 16:49:09 +0100

    Using the new "dart" command

--- tool/coverage.ps1
+++ tool/coverage.ps1
@@ -2,5 +2,5 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-pub run coverage:format_coverage --in=var/test --lcov --out=var/lcov.info --packages=.dart_tool/package_config.json --report-on=lib
-pub run coveralls var/lcov.info
+dart pub run coverage:format_coverage --in=var/test --lcov --out=var/lcov.info --packages=.dart_tool/package_config.json --report-on=lib
+dart pub run coveralls var/lcov.info
